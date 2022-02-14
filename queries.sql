a836777cf6a6896e664b4b249a2f100bbcc4861e
Yigit Mersin <yigitmersin@gmail.com>
2022-02-15 01:46:51 +0300

    Change name & born date queries

--- queries.sql
+++ queries.sql
@@ -1,5 +1,5 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT name FROM animals WHERE name LIKE '%mon';
-SELECT * FROM animals WHERE date_of_birth > '2016-01-01' AND date_of_birth < '2019-12-31';
+SELECT * FROM animals WHERE name LIKE '%mon';
+SELECT name FROM animals WHERE date_of_birth > '2016-01-01' AND date_of_birth < '2019-12-31';
 
