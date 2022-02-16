b36e4ae57248e4f4d455ddd86bf5a52185c51039
Yigit Mersin <yigitmersin@gmail.com>
2022-02-17 01:13:51 +0300

    Insert & update data in animals

--- data.sql
+++ data.sql
@@ -12,3 +12,55 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Boarmon', '2005-06-07', 7, true, -20.4 );
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Blossom', '1998-01-13', 3, true, -17 );
 
+BEGIN;
+SAVEPOINT initialState;
+UPDATE animals SET species='unspecified';
+SELECT * FROM animals;
+ROLLBACK TO initialState;
+SELECT * FROM animals;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+SELECT * FROM animals;
+SAVEPOINT digimon;
+UPDATE animals SET species='pokemon' WHERE species IS NULL;
+SELECT * FROM animals;
+COMMIT;
+
+BEGIN;
+SAVEPOINT backup;
+DELETE FROM animals;
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-06-01';
+SAVEPOINT dateDelete;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO dateDelete;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+SELECT * FROM animals;
+COMMIT;
+
+INSERT INTO owners (full_name,age) VALUES ('Sam Smith' ,'34');
+INSERT INTO owners (full_name,age) VALUES ('Jennifer Orwell' ,'19');
+INSERT INTO owners (full_name,age) VALUES ('Bob' ,'45');
+INSERT INTO owners (full_name,age) VALUES ('Melody Pond' ,'77');
+INSERT INTO owners (full_name,age) VALUES ('Dean Winchester' ,'14');
+INSERT INTO owners (full_name,age) VALUES ('Jodie Whittaker' ,'38');
+
+INSERT INTO species (name) VALUES ('Pokemon');
+INSERT INTO species (name) VALUES ('Digimon');
+
+UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
+UPDATE animals SET species_id = 1 WHERE species_id IS NULL;
+
+UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+UPDATE animals SET owner_id = 2 WHERE name = 'Pikachu';
+UPDATE animals SET owner_id = 2 WHERE name = 'Gabumon';
+UPDATE animals SET owner_id = 3 WHERE name = 'Devimon';
+UPDATE animals SET owner_id = 3 WHERE name = 'Plantmon';
+UPDATE animals SET owner_id = 4 WHERE name = 'Charmander';
+UPDATE animals SET owner_id = 4 WHERE name = 'Squirtle';
+UPDATE animals SET owner_id = 4 WHERE name = 'Blossom';
+UPDATE animals SET owner_id = 5 WHERE name = 'Angemon';
+UPDATE animals SET owner_id = 5 WHERE name = 'Boarmon';
--- queries.sql
+++ queries.sql
@@ -9,35 +9,6 @@ SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name NOT IN ('Gabumon') ;
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 
-BEGIN;
-SAVEPOINT initialState;
-UPDATE animals SET species='unspecified';
-SELECT * FROM animals;
-ROLLBACK TO initialState;
-SELECT * FROM animals;
-UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
-SELECT * FROM animals;
-SAVEPOINT digimon;
-UPDATE animals SET species='pokemon' WHERE species IS NULL;
-SELECT * FROM animals;
-COMMIT;
-
-BEGIN;
-SAVEPOINT backup;
-DELETE FROM animals;
-SELECT * FROM animals;
-ROLLBACK;
-SELECT * FROM animals;
-
-BEGIN;
-DELETE FROM animals WHERE date_of_birth > '2022-06-01';
-SAVEPOINT dateDelete;
-UPDATE animals SET weight_kg = weight_kg * -1;
-ROLLBACK TO dateDelete;
-UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
-SELECT * FROM animals;
-COMMIT;
-
 SELECT count(id) AS total_animals FROM animals;
 SELECT count(id) AS total_no_escape_animals FROM animals;
 SELECT AVG(weight_kg) AS average_weight FROM animals;
