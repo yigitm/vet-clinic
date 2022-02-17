abbedbea509af5397e180d0930ae6874f27ab85c
Yigit Mersin <yigitmersin@gmail.com>
2022-02-17 03:30:32 +0300

    Add join queries

--- queries.sql
+++ queries.sql
@@ -16,10 +16,10 @@ SELECT name, neutered, MAX(escape_attempts) FROM animals GROUP BY name, neutered
 SELECT species, MAX(weight_kg) AS max_weight, MIN(weight_kg) AS min_weight FROM animals GROUP BY species;
 SELECT species, AVG(escape_attempts) AS average_escape FROM animals WHERE DATE_PART('YEAR', date_of_birth) > 1990 AND DATE_PART('YEAR', date_of_birth) < 2000 GROUP BY species;
 
-
-
-
-
-
-
-
+SELECT name FROM animals AS a INNER JOIN owners AS o ON o.id = a.owner_id AND o.full_name = 'Melody Pond';
+SELECT a.name, s.name AS animal_type FROM animals AS a INNER JOIN species AS s ON s.id = a.species_id AND s.id = 1;
+SELECT o.full_name AS owners, a.name AS animals FROM animals AS a RIGHT JOIN owners AS o ON o.id = a.owner_id;
+SELECT count(a.id) AS total_animals, s.name AS animal_type FROM animals AS a RIGHT OUTER JOIN species AS s ON s.id = a.species_id AND a.species_id IS NOT NULL GROUP BY s.name;
+SELECT o.full_name AS owners, a.name AS animal_name, a.species_id AS animal_type FROM animals AS a INNER JOIN owners AS o ON o.id = a.owner_id AND o.full_name = 'Jennifer Orwell';
+SELECT a.name AS animals, o.full_name AS owner FROM animals AS a INNER JOIN owners AS o ON o.id = a.owner_id AND a.escape_attempts = 0 AND o.full_name = 'Dean Winchester';
+SELECT COUNT(a.owner_id) AS total_animal, o.full_name AS owner FROM animals AS a RIGHT OUTER JOIN owners AS o ON o.id = a.owner_id AND a.owner_id >= 1 GROUP BY a.owner_id, o.full_name ORDER BY COUNT(a.owner_id) DESC LIMIT 1;
