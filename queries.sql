f70fcd9158403557380ded8ae703fe1930fef020
Yigit Mersin <yigitmersin@gmail.com>
2022-02-18 01:16:57 +0300

    Merge insert & add first query

--- queries.sql
+++ queries.sql
@@ -9,8 +9,8 @@ SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name NOT IN ('Gabumon') ;
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 
-SELECT count(id) AS total_animals FROM animals;
-SELECT count(id) AS total_no_escape_animals FROM animals;
+SELECT COUNT(id) AS total_animals FROM animals;
+SELECT COUNT(id) AS total_no_escape_animals FROM animals;
 SELECT AVG(weight_kg) AS average_weight FROM animals;
 SELECT name, neutered, MAX(escape_attempts) FROM animals GROUP BY name, neutered, escape_attempts ORDER BY escape_attempts DESC LIMIT 1;
 SELECT species, MAX(weight_kg) AS max_weight, MIN(weight_kg) AS min_weight FROM animals GROUP BY species;
@@ -19,7 +19,14 @@ SELECT species, AVG(escape_attempts) AS average_escape FROM animals WHERE DATE_P
 SELECT name FROM animals AS a INNER JOIN owners AS o ON o.id = a.owner_id AND o.full_name = 'Melody Pond';
 SELECT a.name, s.name AS animal_type FROM animals AS a INNER JOIN species AS s ON s.id = a.species_id AND s.id = 1;
 SELECT o.full_name AS owners, a.name AS animals FROM animals AS a RIGHT JOIN owners AS o ON o.id = a.owner_id;
-SELECT count(a.id) AS total_animals, s.name AS animal_type FROM animals AS a RIGHT OUTER JOIN species AS s ON s.id = a.species_id AND a.species_id IS NOT NULL GROUP BY s.name;
+SELECT COUNT(a.id) AS total_animals, s.name AS animal_type FROM animals AS a RIGHT OUTER JOIN species AS s ON s.id = a.species_id AND a.species_id IS NOT NULL GROUP BY s.name;
 SELECT o.full_name AS owners, a.name AS animal_name, a.species_id AS animal_type FROM animals AS a INNER JOIN owners AS o ON o.id = a.owner_id AND o.full_name = 'Jennifer Orwell';
 SELECT a.name AS animals, o.full_name AS owner FROM animals AS a INNER JOIN owners AS o ON o.id = a.owner_id AND a.escape_attempts = 0 AND o.full_name = 'Dean Winchester';
 SELECT COUNT(a.owner_id) AS total_animal, o.full_name AS owner FROM animals AS a RIGHT OUTER JOIN owners AS o ON o.id = a.owner_id AND a.owner_id >= 1 GROUP BY a.owner_id, o.full_name ORDER BY COUNT(a.owner_id) DESC LIMIT 1;
+
+SELECT animals.name FROM animals 
+JOIN visits ON (animals.id = visits.animals_id) AND visits.vets_id = 1 
+GROUP BY animals.name, visits.date_of_visit
+ORDER BY visits.date_of_visit DESC
+LIMIT 1;
+
